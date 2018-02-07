c3ea6c8566279c336798900e5c24bb001e18d58a
wudi <wodis1988@gmail.com>
2018-02-07 20:31:53 +0800

    优化宠物等级上限脚本

--- src/main/java/com/openwudi/sa/script/PetLevelScript.java
+++ src/main/java/com/openwudi/sa/script/PetLevelScript.java
@@ -16,7 +16,7 @@ import java.io.InputStream;
 import java.util.Scanner;
 
 public class PetLevelScript extends Script {
-    private static final Long ROUND_TIME = 60000L;
+    private static final Long ROUND_TIME = 2 * 60000L;
     private static final OCRFactory OCR_FACTORY = new OCRFactory();
 
     private String imagePath;
@@ -32,13 +32,17 @@ public class PetLevelScript extends Script {
     }
 
     public boolean run() throws IOException {
-        System.out.println("请输入宠物位置:");
+        LogUtil.info("请输入宠物位置(0~4):");
         Scanner sc = new Scanner(System.in);
         String petString = sc.nextLine().trim();
-        System.out.println("请输入限制等级:");
+        LogUtil.info("请输入限制等级:");
         String limitString = sc.nextLine().trim();
+        LogUtil.info("请输入结束时的操作: 1=登出  2=停止遇敌");
+        String optString = sc.nextLine().trim();
         int pet = Integer.parseInt(petString);
         int limit = Integer.parseInt(limitString);
+        int opt = Integer.parseInt(optString);
+        LogUtil.info("宠物位置:{} == 限制等级:{} == 停止方式:{}", pet, limit, opt);
 
         OCR ocr = OCR_FACTORY.getOcr(1);
 
@@ -75,21 +79,8 @@ public class PetLevelScript extends Script {
                     LogUtil.info("等级: {}", level);
                     if (level >= limit) {
                         //打开升级音乐
-                        levelUpBgmStart();
                         LogUtil.info("宠物位置 {}, 到达lv.{}限制!!!", pet, limit);
-                        int times = 0;
-                        while (true) {
-                            adb.tap(1560, 1000);
-                            adb.tap(1600, 830);
-                            adb.tap(1330, 760);
-                            times++;
-                            if (times > 50) {
-                                //关闭升级音乐
-                                levelUpBgmStop();
-                                LogUtil.info("登出并退出程序!!!");
-                                System.exit(0);
-                            }
-                        }
+                        operation(opt);
                     }
                     LogUtil.info("等待 {} 秒后继续检查.", ROUND_TIME / (1000f));
                     Utils.sleep(ROUND_TIME);
@@ -104,7 +95,7 @@ public class PetLevelScript extends Script {
         }
     }
 
-    private void levelUpBgmStart(){
+    private void levelUpBgmStart() {
         new Thread(new Runnable() {
             public void run() {
                 musicPlay.continuousStart();
@@ -112,8 +103,43 @@ public class PetLevelScript extends Script {
         }).start();
     }
 
-    private void levelUpBgmStop(){
+    private void levelUpBgmStop() {
         musicPlay.continuousStop();
+    }
+
+    /**
+     * 达到等级后的操作
+     * @param opt
+     */
+    private void operation(int opt) {
+        levelUpBgmStart();
+        if (1 == opt){
+            int times = 0;
+            while (true) {
+                adb.tap(1560, 1000);
+                adb.tap(1600, 830);
+                adb.tap(1330, 760);
+                times++;
+                if (times > 50) {
+                    //关闭升级音乐
+                    levelUpBgmStop();
+                    LogUtil.info("登出并退出程序!!!");
+                    System.exit(0);
+                }
+            }
+        } else if (2 == opt){
+            int times = 0;
+            while (true) {
+                scriptUtils.stopBattle();
+                times++;
+                if (times > 50) {
+                    //关闭升级音乐
+                    levelUpBgmStop();
+                    LogUtil.info("登出并退出程序!!!");
+                    System.exit(0);
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
